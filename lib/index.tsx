aecc0bc33c39eb0fe5c5e28951426b5ed07691a7
lynn-illumio <101166369+lynn-illumio@users.noreply.github.com>
2022-08-22 21:09:34 -0700

    Config loaders

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,4 @@
 console.log('Hello')
+const div = document.createElement('div')
+div.innerText = 'Hi'
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode: 'production',
     entry: {
         index: './lib/index.tsx'
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
         library: 'BatmanUI',
-        libraryTarget: 'umd'
+        libraryTarget: 'umd',
     },
-    modules: {
+    module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 loader: 'awesome-typescript-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'BatmanUI',
+            template: 'index.html'
+        })
+    ]
+
 }
